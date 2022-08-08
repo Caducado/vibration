1f92b335351a18689e509d45833d71255287620f
Leonardo Nogueira <leo.nog88@gmail.com>
2022-08-08 09:48:05 -0300

    pyenv version

--- SingleDOF.py
+++ SingleDOF.py
@@ -7,25 +7,11 @@ class system():
         self.mass = mass
         self.siffness = stiffness
         self.damping = damping
-        self.position = position
-        self.velocity = velocity
+        self.x_0 = position
+        self.v_0 = velocity
         self.forces = []
         self.omega_n = np.sqrt(self.siffness/self.mass)
     
 
   
-    def harmonic_load(self, frequency, amplitude):
-        self.forces.append(amplitude*np.sin(frequency*self.position))
-        return self.forces
-
-
-    def loads(self, inputs=(.0,.0), gravity_on:bool=True) -> None:
-        if gravity_on:
-            self.forces.append(-self.mass*self.gravity, .0)
-        else:
-            self.forces = []
-        self.force = inputs
-        self.acceleration = inputs / self.mass
-        self.velocity += self.acceleration
-        self.position += self.velocity
-        return self.position
+    
