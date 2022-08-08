6da17b0617c932699cb908593bb23f7273626718
Leonardo Nogueira <leo.nog88@gmail.com>
2022-08-08 14:38:01 -0300

    sub-damped tested

--- test_1DOF.py
+++ test_1DOF.py
@@ -1,6 +1,6 @@
 import SingleDOF as SDOF
 
 
-sys = SDOF.system(mass=1, stiffness=10, damping=1)
+sys = SDOF.system(mass=1, stiffness=1, damping=0.1,position=5, velocity=1)
 
-sys.plot_response(1000, 10000)
+sys.plot_response(100, 10000)
